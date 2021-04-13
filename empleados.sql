ed792524a0a0591b8dc259bec268a3d5284d3ec7
Develoteca <dimit28@gmail.com>
2021-04-13 11:44:33 -0500

    archivos fake api
    Estos archivos "simulan una api" ( ver, insertar, actualizar, editar)

---
+++ empleados.sql
@@ -0,0 +1,66 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: localhost
+-- Tiempo de generación: 13-04-2021 a las 18:43:36
+-- Versión del servidor: 10.4.18-MariaDB
+-- Versión de PHP: 8.0.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `empleados`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `empleados`
+--
+
+CREATE TABLE `empleados` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(255) NOT NULL,
+  `correo` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `empleados`
+--
+
+INSERT INTO `empleados` (`id`, `nombre`, `correo`) VALUES
+(65, 'Oscar', 'oscaruh@develoteca.com');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `empleados`
+--
+ALTER TABLE `empleados`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `empleados`
+--
+ALTER TABLE `empleados`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=66;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ index.php
@@ -0,0 +1,66 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: access");
+header("Access-Control-Allow-Methods: GET,POST");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+// Conecta a la base de datos  con usuario, contraseña y nombre de la BD
+$servidor = "localhost"; $usuario = "root"; $contrasenia = ""; $nombreBaseDatos = "empleados";
+$conexionBD = new mysqli($servidor, $usuario, $contrasenia, $nombreBaseDatos);
+
+
+// Consulta datos y recepciona una clave para consultar dichos datos con dicha clave
+if (isset($_GET["consultar"])){
+    $sqlEmpleaados = mysqli_query($conexionBD,"SELECT * FROM empleados WHERE id=".$_GET["consultar"]);
+    if(mysqli_num_rows($sqlEmpleaados) > 0){
+        $empleaados = mysqli_fetch_all($sqlEmpleaados,MYSQLI_ASSOC);
+        echo json_encode($empleaados);
+        exit();
+    }
+    else{  echo json_encode(["success"=>0]); }
+}
+//borrar pero se le debe de enviar una clave ( para borrado )
+if (isset($_GET["borrar"])){
+    $sqlEmpleaados = mysqli_query($conexionBD,"DELETE FROM empleados WHERE id=".$_GET["borrar"]);
+    if($sqlEmpleaados){
+        echo json_encode(["success"=>1]);
+        exit();
+    }
+    else{  echo json_encode(["success"=>0]); }
+}
+//Inserta un nuevo registro y recepciona en método post los datos de nombre y correo
+if(isset($_GET["insertar"])){
+    $data = json_decode(file_get_contents("php://input"));
+    $nombre=$data->nombre;
+    $correo=$data->correo;
+    $sqlEmpleaados = mysqli_query($conexionBD,"INSERT INTO empleados(nombre,correo) VALUES('$nombre','$correo') ");
+    echo json_encode(["success"=>1]);
+    exit();
+}
+// Actualiza datos pero recepciona datos de nombre, correo y una clave para realizar la actualización
+if(isset($_GET["actualizar"])){
+    
+    $data = json_decode(file_get_contents("php://input"));
+
+    $id=$data->id;
+    $nombre=$data->nombre;
+    $correo=$data->correo;
+    
+    $sqlEmpleaados = mysqli_query($conexionBD,"UPDATE empleados SET nombre='$nombre',correo='$correo' WHERE id='$id'");
+    echo json_encode(["success"=>1]);
+    exit();
+}
+// Consulta todos los registros de la tabla empleados
+$sqlEmpleaados = mysqli_query($conexionBD,"SELECT * FROM empleados ");
+if(mysqli_num_rows($sqlEmpleaados) > 0){
+    $empleaados = mysqli_fetch_all($sqlEmpleaados,MYSQLI_ASSOC);
+    echo json_encode($empleaados);
+}
+else{ echo json_encode([["success"=>0]]); }
+
+
+
+
+
+?>
